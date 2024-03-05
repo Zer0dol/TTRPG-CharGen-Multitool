cefcebd4c83e43a4a163e49ad42fc024a21c1f7e
Zer0dol <59622513+Zer0dol@users.noreply.github.com>
2024-03-06 02:11:10 +0500

    Fresh upload
    Brand new main branch of TTRPGCharGen on C# with WPF

---
+++ WPFTTRPGCharGen/Pages/Cyberpunk2020.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTTRPGCharGen.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Cyberpunk2020.xaml
+    /// </summary>
+    public partial class Cyberpunk2020 : Page
+    {
+        public Cyberpunk2020()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFTTRPGCharGen/Pages/MenuPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTTRPGCharGen.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+        private void ShadowrunButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new Uri("../Pages/Shadowrun5E.xaml", UriKind.Relative));
+        }
+
+        private void DnDButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new Uri("../Pages/DnD5E.xaml", UriKind.Relative));
+        }
+
+        private void CyberpunkButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new Uri("../Pages/Cyberpunk2020.xaml", UriKind.Relative));
+        }
+
+        private void WoDButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new Uri("../Pages/World of Darkness.xaml", UriKind.Relative));
+        }
+    }
+}
